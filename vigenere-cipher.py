6ebd5980527a63e9a871fdcf0329b3e5f9e6d5a6
Andrew Maurice <andromulin@gmail.com>
2019-03-20 09:31:52 -0500

    init commit
    added Gourd's writeup file and a Python 2.7 file to start coding in (header already setup)

---
+++ vigenere-cipher.py
@@ -0,0 +1,14 @@
+################################################################
+# Authors: Team Chinese (Lane Arnold, Christopher Boquet,
+# 	   Christopher Bouton, Darrell Durousseaux, Clay Fonseca,
+#	   Rebecca Grantham, Andrew Maurice)
+# Class: CSC 442
+# Date: 3-29-2019
+# Description: Program 2: Vigenere Cipher
+#              The Python code will read either a plaintext or
+#              ciphertext from stdin and send the generated
+#              output file to stdout. The mode (-e to encrypt or
+#              -d to decrypt) and the key must be provided via
+#              the command line for the code to work properly.
+################################################################
+
