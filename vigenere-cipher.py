1d21d00c77876447a8b893d9c64b5720cddc3f35
Andrew Maurice <acm1264@yahoo.com>
2019-03-20 20:29:28 -0500

    functions laid out and basic terminal functionality
    Functions for encrypt and decrypt are set as placeholders (functionality
    not  yet added). Taking in parameters from the terminal and reading a
    line from stdin (be it text or a file) works.

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -13,3 +13,37 @@
 #              the command line for the code to work properly.
 ################################################################
 
+import sys
+###############################################################
+
+#take in key with the plaintext to encrypt the message
+def encrypt(string, key):
+	return "encrypt " + string + " " + key
+
+#take in key with ciphertext to decrypt the message
+def decrypt(string, key):
+	return "decrypt " + string + " " + key
+
+
+
+#confirm proper usage of program
+if(len(sys.argv) < 3):
+	print("Usage: python vigener-cipher.py -[d or e] KEY")
+	exit()
+
+#determine mode
+if(sys.argv[1] == "-d"):
+	#process all inputs from stdin as ciphertext
+	####need to add ability to loop until interrupt
+	text = sys.stdin.readline().split('\n')
+	print(decrypt(text[0], sys.argv[2]))
+
+elif(sys.argv[1] == "-e"):
+	#process all inputs from stdin as plaintext
+	####looping here too
+	text = sys.stdin.readline().split('\n')
+	print(encrypt(text[0], sys.argv[2]))
+
+#report improper usage
+else:
+	print("Usage: python vigener-cipher.py -[d or e] KEY")
