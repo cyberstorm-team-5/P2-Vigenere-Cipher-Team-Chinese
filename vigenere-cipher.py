7a8c4ab219db006f1b82d8705dbe3fc359ad73ac
Andrew Maurice <acm1264@yahoo.com>
2019-03-26 21:09:39 -0500

    cleaning up
    added back the .py extension for consistency across repositories.
    Cleaned up spacing and tested in Linux to ensure everything works
    properly.

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -19,140 +19,126 @@ import string
 ###############################################################
 
 #string of all lowercase and uppercase letters (in that order)
-
 alph = string.ascii_letters
+
 
 #take an array of numbers (and non-alphabet symbols) to convert to
 #the new text (either plain or cipher)
-
 def newText(code):
         result = ""
         for num in code:
         
                 if(isinstance(num, int)):
-                
                         #ints in the code array match to indexes of corresponding letters in alph,
                         #so that letter is appended to the text
-                        
                         result += alph[num]
+                        
                 else:
-                
                         #all other indexes in code array will be another array that holds a
                         #non-alphabetic symbol from the inputted text that is preserved to
                         #insert directly into the new text
-                        
                         result += num[0]
+                        
         return result
 
-#take spaces out of key
 
+#take spaces out of key
 def cleanKey(key):
         key = key.split()
         key = "".join(key)
         return key
 
-#take in key with the plaintext to encrypt the message
 
+#take in key with the plaintext to encrypt the message
 def encrypt(string, key):
 
         #int showing how many non-alphabetic symbols have been processed. Because
         #those symbols are not modified in Vigenere cipher, the key should not advance
         #an index after processing one, so this int compensates for the offset between
         #the index used for the key and the one used for the index of the string
-        
         nonAlph = 0
         cipher = []
         key = cleanKey(key)
 
         #encrypt one index of the inputted string at a time
-        
         for i in range(len(string)):
                 
                 #check if a non-alphabetic symbol is present
-                
                 if(not (string[i] in alph)):
                 
                         #append the non-alphabetic symbol and increment
                         #the counter to keep the key index accurate
-                        
                         cipher.append([string[i]])
                         nonAlph += 1
+                        
                 else:
-                
+
                         #use encryption equation for vigenere
                         #[ciphertext at index i =(plaintext at i + key at i)%26]
                         #where, to compensate for the key possibly being shorter than the text, the
                         #modulus of the key's length is taken to loop back around, but before that the
                         #amount of non-alphabetic symbols is subtracted from it to compensate for
                         #symboles liek numbers and spacing not advancing the key index
-                        
                         index = ((alph.index(string[i]) + alph.index(key[((i-nonAlph) % len(key))])) % 26)
 
                         #for uppercase letter, add 26 to go from lowercase's index to uppercase's
-                        
                         if(string[i].isupper()):
                                 index += 26
+                                
                         cipher.append(index)
 
         #send resulting ciphertext to stdout
-        
         print (newText(cipher))
 
-#take in key with ciphertext to decrypt the message
 
+#take in key with ciphertext to decrypt the message
 def decrypt(string, key):
 
         #same purpose as in encryption
-        
         nonAlph = 0
         plain = []
         key = cleanKey(key)
         
         #decrypt one index of the inputted string at a time
-        
         for i in range(len(string)):
                 
                 #check if a non-alphabetic symbol is present
-                
                 if(not (string[i] in alph)):
                         plain.append([string[i]])
                         nonAlph += 1
+                        
                 else:
                         #use decryption equation for vigenere
                         #[plaintext at index i =(26 + ciphertext at i - key at i)%26]
                         #where the same compensation for non-alphabetic symbols is applied to the
                         #key, and 26 is added overall to ensure teh result of the subtraction will
                         #never be negative without impacting modulus
-                        
                         index = ((26 + alph.index(string[i]) - alph.index(key[((i-nonAlph) % len(key))])) % 26)
 
                         #for uppercase letter, add 26 to go from lowercase's index to uppercase's
-                        
                         if(string[i].isupper()):
                                 index += 26
+                                
                         plain.append(index)
 
         #send resulting plaintext to stdout
-        
         print (newText(plain))
+
         
 #confirm proper usage of program
-
 if(len(sys.argv) < 3):
-	print("Usage: ./vigener-cipher -[d or e] KEY")
+	print("Usage: ./vigener-cipher.py -[d or e] KEY")
 	exit()
 
-#determine mode
 
+#determine mode
 if(sys.argv[1] == "-d"):
 
 	#process all inputs from stdin as ciphertext
-    
         text = sys.stdin.readline()
         while(text):
         
                 #split off the newline before sending to decrypt
-                
                 text = text.split('\n')
                 decrypt(text[0], sys.argv[2])
                 text = sys.stdin.readline()
@@ -160,17 +146,14 @@ if(sys.argv[1] == "-d"):
 elif(sys.argv[1] == "-e"):
 
 	#process all inputs from stdin as plaintext
-    
 	text = sys.stdin.readline()
         while(text):
         
                 #split off the newline before sending to encrypt
-                
                 text = text.split('\n')
                 encrypt(text[0], sys.argv[2])
                 text = sys.stdin.readline()
 
 #report improper usage
-
 else:
-	print("Usage: ./vigener-cipher -[d or e] KEY")
+	print("Usage: ./vigener-cipher.py -[d or e] KEY")
